ea41a7bb6d3cf5605568eac813e901278fba0f16
Lee <ljs0429777@naver.com>
2016-12-30 19:49:57 +0900

    Test Commit 2

--- SwiftStudio/ViewController.swift
+++ SwiftStudio/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         // TEST
         // Test Commit
+        // Test2
     }
 
     override func didReceiveMemoryWarning() {
