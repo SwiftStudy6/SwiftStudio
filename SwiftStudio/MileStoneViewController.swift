10df03f3ece0be74b7fbc6524dd7db7b09a56c26
홍대호 <hongdaeho@172.20.nate.com>
2017-01-13 09:20:58 +0900

    commit milestone

--- SwiftStudio/MileStoneViewController.swift
+++ SwiftStudio/MileStoneViewController.swift
@@ -9,5 +9,22 @@
 import Foundation
 import Firebase
 
+class MildStoneViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
 
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
 
+    
+    
+}
