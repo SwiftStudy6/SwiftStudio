017bbbc090beed26e6bfa9252d819947dc8105a2
강준 <wajl1004@gmail.com>
2017-01-25 04:11:10 +0900

    Autolayout을 위한 Extension추가

--- SwiftStudio/MainViewController.swift
+++ SwiftStudio/MainViewController.swift
@@ -826,3 +826,19 @@ extension NSDate {
         return formater.string(from: self as Date)
     }
 }
+
+extension UIView {
+    func addConstraintWithFormat(_ format : String, _ views : UIView...){
+        
+        var viewsDictionary = [String : UIView]()
+        
+        for (index, view) in views.enumerated() {
+            let key = "v\(index)"
+            viewsDictionary[key] = view
+            view.translatesAutoresizingMaskIntoConstraints = false
+        }
+        
+        
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: format, options: NSLayoutFormatOptions(), metrics: nil, views: viewsDictionary))
+    }
+}
