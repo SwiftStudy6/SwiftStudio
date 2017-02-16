456c9ff0d995372a753ffae96a451bfafd463ba2
강준 <wajl1004@gmail.com>
2017-02-16 16:39:30 +0900

    권고사항 추가
    1. AppDelegate내 로그인 뷰 루트 고치지 말아주세요
     - 로그인 디버그 플래그 추가해서 커밋 합니다 가능하면 그렇게 사용해주세요 (권한테스트로 인함)
    2. 그룹 뷰어 추가 하기 위해서 파일만 올려놓습니다.
     - 아직 그림만 그려놓고 있어요 (삭제 권한에 대한 고민이 있습니다)

--- SwiftStudio/GroupViewController.swift
+++ SwiftStudio/GroupViewController.swift
@@ -16,9 +16,6 @@ class GroupObject : NSObject {
     var manager : Array<String>! = nil //Group ManagerList
     var createTime : Double! = 0.0
     var editTime   : Double! = 0.0
-    
-    
-    
 }
 
 
@@ -111,34 +108,7 @@ class GroupViewController: UICollectionViewController {
     }
 
     // MARK: UICollectionViewDelegate
-
-    /*
-    // Uncomment this method to specify if the specified item should be highlighted during tracking
-    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
-        return true
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
     }
-    */
-
-    /*
-    // Uncomment this method to specify if the specified item should be selected
-    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
-    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
-    
-    }
-    */
-
 }
