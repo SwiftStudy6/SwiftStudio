41c09635d2e1f1b5845466c5d6484eb0a6eb87ee
hongdaeho <ghdeo2167@gmail.com>
2017-04-07 19:50:22 +0900

    Revert "commit"
    This reverts commit a395eb36be70464b526bc74c84b54a6a62e5da18.

--- SwiftStudio/MileStoneViewController.swift
+++ SwiftStudio/MileStoneViewController.swift
@@ -175,8 +175,6 @@ class MileStoneViewController: UIViewController, UITableViewDataSource, UITableV
     }
     
     
-    
-    
     public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
     {
         
